c227a514f01abb0302020e1b6eff7770c6529dc6
Sarah Strange <strange1120@gmail.com>
2022-12-02 00:48:56 -0500

    chore: remove test

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@ mod utils;
 #[shuttle_service::main]
 async fn axum() -> shuttle_service::ShuttleAxum {
     let router = Router::new().route("/getNFT/mint/:id/network/:network", get(fetch_nft_handler));
-
     let sync_wrapper = SyncWrapper::new(router);
 
     Ok(sync_wrapper)
